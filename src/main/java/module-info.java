8f48e556213e77bcc7cee36b567629f97e61e65a
anicaaovo <93107633+anicaaovo@users.noreply.github.com>
2022-11-01 01:38:21 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.projectthree {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.projectthree to javafx.fxml;
+    exports com.example.projectthree;
+}
